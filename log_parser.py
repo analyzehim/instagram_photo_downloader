3367a0cdd313b3d37527d841bdc27c78a19fd0c4
analyzehim <raevegnforspam@rambler.ru>
2017-12-05 10:41:10 +0300

    12 GODDAMN HOURS MOTHERHECKING FIX

--- log_parser.py
+++ log_parser.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
         telebot = Telegram()
         log = logDB()
         try:
-            if datetime.datetime.now().hour == 24:
+            if datetime.datetime.now().hour == 12:
                 telebot.send_text(telebot.admin_id, "{0} unique users in the last 24 hours".format(log.get_status(24)))
                 time.sleep(60 * 60)
 
